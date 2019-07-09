79672f177e1c7eeb475578b5334312d14113a362
Zorioux <50109267+Zorioux@users.noreply.github.com>
2019-07-09 15:58:36 +0300

    Update command.java

--- Uncraft level 1/src/main/java/uncraft/mythicmc/zorioux/uncraft/command.java
+++ Uncraft level 1/src/main/java/uncraft/mythicmc/zorioux/uncraft/command.java
@@ -12,9 +12,9 @@ import org.bukkit.inventory.PlayerInventory;
 import org.bukkit.plugin.Plugin;
 
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+
+import java.util.HashMap;
+
 
 
 
@@ -31,19 +31,47 @@ public class command implements CommandExecutor {
 
 
         if (command.getName().equalsIgnoreCase("uncraft")){
-          if (sender instanceof  Player) {
-              Player p = (Player) sender;
 
-                    if (!(p.getInventory().getItemInMainHand() == null) &&
-                            !(p.getInventory().getItemInOffHand() == null))
-                    {
-                            //gets the size of un-craft path in config
-                        int toUncraft = plugin.getConfig().getList("un-craft").size();
-                        p.sendMessage("uncraft list size is :  " + toUncraft);
-                        //
-                    if (toUncraft > 0) {
+          if (!(sender instanceof  Player)) {
+
+
+              plugin.getServer().getConsoleSender().
+                      sendMessage(ChatColor.RED + "only players may execute this command you dum dum!");
+              return false;
+          }
+
+              Player p = (Player) sender;
+                if (!(p.hasPermission("mythic.uncraft.use"))){
+                    p.sendMessage(ChatColor.DARK_RED + "you don't have permission to use this command");
+                    return false;
+                }
+
+                  //gets the size of un-craft path in config
+                  int toUncraft = plugin.getConfig().getStringList("un-craft").size();
+
+
+                // if config has no set in it stops code here
+                if (toUncraft == 0) {p.sendMessage(ChatColor.BOLD + " there is no items in config to be un-crafted");
+                  plugin.getServer().getConsoleSender().sendMessage(ChatColor.BOLD + " there is no items in config to be un-crafted");
+                   return false;
+              }
+
+
+                            // makes sure that player has an item in hand so less memory usage
+                            // instead of wasting it scanning air item
+                    if (p.getInventory().getItemInMainHand().getType().equals(Material.getMaterial("AIR")) &&
+                            p.getInventory().getItemInOffHand().getType().equals(Material.getMaterial("AIR")))
+                    {       p.sendMessage(ChatColor.BOLD + "you don't have item in your hand or off hand");
+                        return false;
+                    }
+
+
+
+
+
+
                         // loop to check if any of the items in the list is in player's hand
-                                int o = 0;
+
                     for (int f = 0 ; f < toUncraft ; f++) {
 
                         // splits item to be un-crafted from drops
@@ -51,10 +79,21 @@ public class command implements CommandExecutor {
 
 
 
-                    if (p.getInventory().getItemInMainHand().getType().equals(Material.valueOf(items[0].toUpperCase())) ||
-                            p.getInventory().getItemInOffHand().getType().equals(Material.valueOf(items[0].toUpperCase()))
-                    )  {
-                            o++;
+                    if (!(p.getInventory().getItemInMainHand().getType().equals(Material.valueOf(items[0].toUpperCase()))
+
+                            )
+                    ) {
+                        if (!(p.getInventory().getItemInOffHand().getType().equals(Material.valueOf(items[0].toUpperCase())))){
+
+                        if (f + 1 == toUncraft ){ p.sendMessage(ChatColor.BOLD + "the item in your hand " +
+                                "can not be uncrafted");}
+
+                          continue;
+
+                    }}
+
+
+
 
                        // gets full string of drops with their quantities
                     String allDrops = items[1];
@@ -64,17 +103,17 @@ public class command implements CommandExecutor {
                     int dropsQuantity = dropsArray.length;
                      // splits drops from their quantities
 
-                List<String> dropsAndQuantities = new ArrayList<>();
+                HashMap<String, String> dropsAndQuantities = new HashMap<>();
 
 
-                    for (int w = 0 ; w < dropsQuantity ; w++) {
+                    for (int w = 0 ; w < dropsQuantity  ; w++) {
                         String[ ] dropQ = dropsArray[w].split("#");
-                        dropsAndQuantities.add(dropQ[w]);
-                        dropsAndQuantities.add(dropQ[w+1]);
+
+                        dropsAndQuantities.put(dropQ[0], dropQ[1]);
+
 
                     }
-                    /* now by this every odd number 0 2 4 ... in dropz[#]  is an item to be dropped
-                     * and every odd number 1 3 5 ... in dropz[#]  is item's quantity to drop */
+                    /* by this each key is item to be dropped and it's value is how many to be dropped */
 
 
 
@@ -91,77 +130,68 @@ public class command implements CommandExecutor {
 
 
 
-                    //check saddle in main hand and if there is space in player's inventory
+                    //check item in main hand and if there is space in player's inventory
               if(p.getInventory().getItemInMainHand().getType().equals(Material.valueOf(items[0].toUpperCase())) &&
-                      dropsQuantity <= (slots -1)) {
-                  //removes saddle
-                 p.getInventory().setItemInMainHand(null);
+                      dropsQuantity <= (slots +1)) {
 
+                            if (p.getInventory().getItemInMainHand().getAmount() > 1 && !(dropsQuantity <= slots)){
+                                p.sendMessage(ChatColor.BOLD + "you don't have enough space in your inventory!!!");
+                                return false;
+                            }
+                              //removes item
+                            p.getInventory().removeItem(new ItemStack(Material.matchMaterial(items[0]), 1));
+                            p.updateInventory();
 
                     //gives items
                   PlayerInventory inventory = p.getInventory();
-                    for (int y = 0; y < dropsQuantity ; y++) {
+                    for (String i : dropsAndQuantities.keySet()) {
 
-                        String drop = dropsAndQuantities.get(y + y);
-                        String drop2 = dropsAndQuantities.get(y + y + 1);
-                        int quantity = Integer.parseInt(drop2);
 
-                 inventory.addItem(new ItemStack(Material.valueOf(drop.toUpperCase()), quantity));
+
+                       int quantity = Integer.parseInt(dropsAndQuantities.get(i));
+                 inventory.addItem(new ItemStack(Material.valueOf(i.toUpperCase()), quantity ));
+
+
                     }
                   p.sendMessage(ChatColor.DARK_GREEN+ "uncrafted successfully");
-
-              }          //check saddle off main hand and if there is space in player's inventory
+                  break;
+              }          //check item off main hand and if there is space in player's inventory
               else if(p.getInventory().getItemInOffHand().getType().equals(Material.valueOf(items[0].toUpperCase())) &&
-                      dropsQuantity <= (slots -1)) {
-                  //removes saddle
-                  p.getInventory().setItemInOffHand(null);
+                      dropsQuantity <= (slots +1)) {
 
+                  //removes item
+                  if (p.getInventory().getItemInOffHand().getAmount() > 1 && !(dropsQuantity <= slots)){
+                      p.sendMessage(ChatColor.BOLD + "you don't have enough space in your inventory!!!");
+                      return false;
+                  }
+                  //removes item
+                  p.getInventory().getItemInOffHand().setAmount(p.getInventory().getItemInOffHand().getAmount() - 1);
+                  p.updateInventory();
 
                   //gives items
+
+
                   PlayerInventory inventory = p.getInventory();
+                  for (String i : dropsAndQuantities.keySet()) {
 
-                  for (int y = 0; y < dropsQuantity ; y++) {
 
-                      String drop = dropsAndQuantities.get(y + y);
-                      String drop2 = dropsAndQuantities.get(y + y + 1);
-                      int quantity = Integer.parseInt(drop2);
+                      int quantity = Integer.parseInt(dropsAndQuantities.get(i));
+                      inventory.addItem(new ItemStack(Material.valueOf(i.toUpperCase()), quantity ));
 
-                      inventory.addItem(new ItemStack(Material.valueOf(drop.toUpperCase()), quantity));
+
                   }
 
                   p.sendMessage(ChatColor.DARK_GREEN+ "uncrafted successfully");
-
+                  break;
                   //send msg if player's inventory is full
-              }else if (dropsQuantity > (slots -1)){ p.sendMessage(ChatColor.BOLD +
+              }else if (dropsQuantity > (slots +1)){ p.sendMessage(ChatColor.BOLD +
                       "you don't have enough space in your inventory!!!");
+                  break;
 
-                  //if all failed , there is no saddle in player hands outputs msg
-              } else if(!p.getInventory().getItemInOffHand().getType().equals(Material.valueOf(items[0].toUpperCase())) &&
-                      !p.getInventory().getItemInMainHand().getType().equals(Material.valueOf(items[0].toUpperCase())))
-              {p.sendMessage(ChatColor.BOLD  + "you don't have saddle in your hand");}
-
-
-              else {p.sendMessage(ChatColor.BOLD + "Error , please report this Error to Zorioux");}
-
-
-          }else if (o == 0 && f == toUncraft){
-                        p.sendMessage(ChatColor.BOLD + "this items in your hand or off hand can not be un-crafted");
-                    }
-                    }
-                    }else if (toUncraft == 0) {p.sendMessage(ChatColor.BOLD + " there is no items in config to be un-crafted");
-                    plugin.getServer().getConsoleSender().sendMessage(ChatColor.BOLD + " there is no items in config to be un-crafted");}
-          } else if (p.getInventory().getItemInMainHand().getType().equals(Material.AIR) && p.getInventory().getItemInOffHand().getType().equals(Material.AIR)){
-                        p.sendMessage(ChatColor.BOLD + " you don't have any item in your hand");
-                    }
-
-        } else {plugin.getServer().getConsoleSender().
-                  sendMessage(ChatColor.RED + "only players may execute this command you dum dum!");}
-
-        }
-
-
-
-        return false;}}
+              } else {p.sendMessage(ChatColor.BOLD + "Error , please report this Error to Zorioux");
+                  break;
+              }}}
+                    return false;}}
 
 
 
